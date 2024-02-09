a54e11ec2866f40e22a18af5eb65577bb4dd3dba
Vineeth Rai K <vineethraik23082000@gmail.com>
2024-02-09 22:25:40 +0530

    Add redirect on requesting "/"

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.redirect("/site");
+});
 app.use("/site", express.static(path.join(__dirname, "./static/site")));
 app.get("/site/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./static/site", "index.html"));
