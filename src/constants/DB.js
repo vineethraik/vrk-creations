da67ca771df39537494eef510a0fde3392af43a0
Vineeth Rai K <vineethraik23082000@gmail.com>
2024-07-01 03:09:14 +0530

    Add constants for contact us

---
+++ src/constants/DB.js
@@ -0,0 +1,5 @@
+export const collections = {
+  CONTACT_US_SETTINGS: "contact_us_settings",
+  CONTACT_US_USERS: "contact_us_users",
+  CONTACT_US_LOG: "contact_us_log",
+};
---
+++ src/constants/contactUs.js
@@ -0,0 +1,14 @@
+export const requestTargets = {
+  PERSON: "person",
+  DESIGNATION: "designation",
+  GENERAL: "general",
+  ALL: "all",
+};
+
+export const socialMediaTypes = {
+  WHATSAPP: "whatsapp",
+  INSTAGRAM: "instagram",
+  TWITTER: "twitter",
+  SNAPCHAT: "snapchat",
+  FACEBOOK: "facebook",
+};
