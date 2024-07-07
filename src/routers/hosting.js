891881905b6f1b7ad1773bc5f7c9728948f0354c
Vineeth Rai K <vineethraik23082000@gmail.com>
2024-07-07 16:38:42 +0530

    Add hosting routes

--- src/routers/hosting.js
+++ src/routers/hosting.js
@@ -1,1 +1,50 @@
+import express, { Router } from "express";
+import { collections } from "../constants/DB.js";
 import { db } from "../services/mongoDB.js";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+
+const hostingRouter = Router();
+
+let hostingSet = await db
+  .collection(collections.HOSTING_SITES_DATA)
+  .find({})
+  .toArray();
+
+hostingSet.map((hostingData) => {
+  hostingRouter.use(
+    `/${hostingData?._id}`,
+    express.static(path.join(__dirname, `../../static/hosting/${hostingData?._id}`))
+  );
+  console.log(`/${hostingData?._id}`);
+  if (hostingData?.project.type === "react") {
+    hostingRouter.get(`/${hostingData?._id}/*`, (req, res) => {
+      res.sendFile(
+        path.join(
+          __dirname,
+          `../../static/hosting${hostingData?._id}`,
+          `${hostingData?.project?.entry}`
+        )
+      );
+    });
+  } else if (hostingData?.project.type === "html") {
+    hostingRouter.get(`/${hostingData?._id}/`, (req, res) => {
+      res.sendFile(
+        path.join(
+          __dirname,
+          `../../static/hosting/${hostingData?._id}`,
+          `${hostingData?.project?.entry}`
+        )
+      );
+    });
+  }
+});
+// console.log(hostingRouter.stack.map((route) => route.path || route.regexp));
+
+
+export default hostingRouter;
